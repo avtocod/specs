c47593ba0ffb0c5cb370b04f3464f0e6c0e3f422
Paramtamtam <slashkot@gmail.com>
2018-11-23 17:24:52 +0500

    v2.10.0

---
+++ sdk/php/src/Structures/VehicleMark.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Avtocod\Specifications\Structures;
+
+use Traversable;
+
+class VehicleMark extends AbstractStructure
+{
+    /**
+     * Name.
+     *
+     * @var string|null
+     */
+    protected $name;
+
+    /**
+     * Unique identifier.
+     *
+     * @var string|null
+     */
+    protected $id;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function toArray()
+    {
+        return [
+            'name' => $this->name,
+            'id'   => $this->id,
+        ];
+    }
+
+    /**
+     * Get unique identifier.
+     *
+     * @return null|string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get name.
+     *
+     * @return null|string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure($raw_data)
+    {
+        if (\is_array($raw_data) || $raw_data instanceof Traversable) {
+            foreach ($raw_data as $key => $value) {
+                switch ($key) {
+                    case 'name':
+                        $this->name = $value === null
+                            ? null
+                            : (string) $value;
+                        break;
+
+                    case 'id':
+                        $this->id = $value === null
+                            ? null
+                            : (string) $value;
+                        break;
+                }
+            }
+        }
+    }
+}
